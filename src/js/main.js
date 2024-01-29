fccaa307906f7664ea92bdc648952d18e91a430a
Informatica <informatica@alumno.local>
2024-01-29 11:09:48 -0600

    Se actualiza el login

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,27 @@
 // main.js
-const User = require('./user'); 
+const user = require('./user'); 
 
 // crear y almacenar usuarios
 const usuarios = [
-    new User("juan123", "correo1@example.com", "contraseña123"),
-    new User("maria456", "correo2@example.com", "otraContraseña456"),
+    new user("juan", "juan@example.com", "123"),
+    new user("maria", "maria@example.com", "456"),
+    new user("otto", "otto@example.com", "789"),
     
 ];
 
 
 // Imprimir la lista de usuarios
 console.log("Lista de usuarios:", usuarios);
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    const ingresarBtn = form.querySelector('input[value="Ingresar"]');
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        handleFormSubmit();
-    });
-
-    ingresarBtn.addEventListener('click', function (event) {
-        event.preventDefault();
-        handleFormSubmit();
-    });
-
-    function handleFormSubmit() {
-        const username = form.elements['txtUser'].value;
-        const password = form.elements['txtPass'].value;
-
-        // Aquí puedes realizar la lógica de autenticación o enviar los datos al servidor
-        console.log('Usuario:', username);
-        console.log('Contraseña:', password);
-    }
-});
-
-
-
-/*
-// Simula un proceso de inicio de sesión
-const usuarioInput = "juan123";
-const contraseñaInput = "contraseña123";
+const uss = "juan";
+const pss = '123';
 
 // Buscar el usuario en la lista
-const usuarioEncontrado = usuarios.find(user => user.getUser() === usuarioInput);
+const usuarioEncontrado = usuarios.find(user => user.getUser() === uss);
 
-if (usuarioEncontrado && usuarioEncontrado.validateLogin(usuarioInput, contraseñaInput)) {
+if (usuarioEncontrado && usuarioEncontrado.validateLogin(uss, pss)) {
     console.log("Inicio de sesión exitoso");
 } else {
     console.log("Credenciales incorrectas");
 }
 
-*/
