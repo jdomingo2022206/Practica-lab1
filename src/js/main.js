05e82528d35b2c7a89ac61c82560f3d7a52a36b9
Jonathan <jdomingo-2022206@kinal.edu.gt>
2024-02-02 15:05:49 -0600

    cargar datos perfil

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,20 @@
+
+let ussEncontrado;
+
 function start() {
   const usuarios = [
     { user: "juan", mail: "juan@example.com", pass: "123" },
     { user: "maria", mail: "maria@example.com", pass: "456" },
     { user: "otto", mail: "otto@example.com", pass: "789" },
   ];
-  const uss = document.getElementById("txtUser").value;
-  const pss = document.getElementById("txtPass").value;
+  let uss = document.getElementById("txtUser").value;
+  let pss = document.getElementById("txtPass").value;
   log(usuarios, uss, pss);
 }
 
 function log(usuarios, uss, pss) {
   const usuarioEncontrado = usuarios.find((user) => user.user === uss);
-  
+  ussEncontrado = usuarioEncontrado;
   if (usuarioEncontrado && usuarioEncontrado.pass === pss) {
     console.log("Inicio de sesión exitoso");
     window.location.href = "./src/html/principal.html";
@@ -19,4 +22,15 @@ function log(usuarios, uss, pss) {
     alert("Sus credenciales son incorrectas o no existen, por favor intente de nuevo.");
     console.log("Credenciales incorrectas");
   }
+}
+
+function register() {
+  //window.location.href = "./src/html/register.html";
+}
+
+function profile(){
+  //document.getElementById("ussName").innerText = ussEncontrado.user;
+  document.getElementById("ussName").innerText = "juan";
+  document.getElementById("ussId").innerText = "@juan";
+  document.getElementById("ussEmail").innerText = "juan@example.com";
 }
